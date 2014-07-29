577fddd014bc82d3b65497bf5e320efab3fa135d
ashu3445 <Asawari.Raste@cumminscollege.in>
2014-07-29 12:46:50 +0530

    Update calc.cpp

--- calc.cpp
+++ calc.cpp
@@ -2,8 +2,10 @@
 void main()
 {
 cout<<"Welcome to Git Hub";
+cout<<"this is a calculator";
 }
 double (double in_number1,double in_number2)
 {
   return number1+number2;
+  cout<<"returning a number";
 }
